e5cbf007a04fd3db992a5156408a71399588d6a5
Nico Bustelo <nbustelo2002@gmail.com>
2022-07-05 02:28:20 -0300

    Dependacies change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const lodash = require('lodash');
 
-const items = [1, [1, [2, [3, [4]]]]];
+const items = [1, [2, [3, [4]]]];
 const newItems = lodash.flattenDeep(items);
 console.log(newItems);
+console.log('Hello Man');
